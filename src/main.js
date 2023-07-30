1a08616507ce95d4237d859f08c0d4b325aa50f7
rakasiwisurya <rakasiwisurya.nutech@gmail.com>
2023-07-30 12:45:02 +0700

    done preview weather

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
+import PerfectScrollbar from "vue3-perfect-scrollbar";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwindcss.css";
+import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PerfectScrollbar);
 
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,80 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapBoxSearchResults"
+      >
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p v-if="!searchError && mapBoxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapBoxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapBoxSearchResults = ref(null);
+const searchError = ref(null);
+
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.trim(), city: city.trim() },
+    query: {
+      lng: searchResult.geometry.coordinates[0],
+      lat: searchResult.geometry.coordinates[1],
+      preview: true,
+    },
+  });
+};
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${
+            searchQuery.value
+          }.json?access_token=${import.meta.env.VITE_MAPBOX_API_KEY}&types=place`
+        );
+        mapBoxSearchResults.value = result.data.features;
+      } catch (error) {
+        console.error(error);
+        searchError.value = true;
+      }
+      return;
+    }
+    mapBoxSearchResults.value = null;
+    clearTimeout(queryTimeout.value);
+  }, 300);
+};
+</script>

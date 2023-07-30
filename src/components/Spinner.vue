77dbfdb0334dcdad76f13c773fc593d8f09e6abc
rakasiwisurya <rakasiwisurya.nutech@gmail.com>
2023-07-30 13:16:41 +0700

    done make spinner using tailwind

---
+++ src/components/Spinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="inline-block w-12 h-12 border-r-transparent border-4 border-white border-solid rounded-full animate-spin"
+  >
+    <span class="absolute h-1 w-1 p-0 m-n1 overflow-hidden clip rect-0 whitespace-nowrap border-0">
+      Loading...
+    </span>
+  </div>
+</template>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -4,7 +4,9 @@
       <AsyncCityView />
 
       <template #fallback>
-        <p>Loading...</p>
+        <div class="flex items-center justify-center h-screen">
+          <Spinner />
+        </div>
       </template>
     </Suspense>
   </div>
@@ -12,4 +14,21 @@
 
 <script setup>
 import AsyncCityView from "@/components/AsyncCityView.vue";
+import Spinner from "../components/Spinner.vue";
 </script>
+
+<style scoped>
+@keyframes pulse {
+  0%,
+  100% {
+    opacity: 0.6;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+.animate-pulse {
+  animation: pulse 1.5s infinite;
+}
+</style>
